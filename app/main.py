b56b07a43c5c49a470f944b04aadd105be0661f6
max31ru12 <maxevg72@gmail.com>
2025-06-21 22:34:51 +0300

    feat: Added CORS for frontend

--- app/main.py
+++ app/main.py
@@ -2,10 +2,11 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
+from starlette.middleware.cors import CORSMiddleware
 
 from app.core.config import DEV_MODE
-from app.domains.users.api import router as users_router
 from app.domains.auth.api import router as auth_router
+from app.domains.users.api import router as users_router
 
 
 @asynccontextmanager
@@ -23,6 +24,22 @@ app = FastAPI(
 
 app.include_router(users_router, prefix="/api/users")
 app.include_router(auth_router, prefix="/api/auth")
+
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:8000",
+]
+
+# Настройка CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,  # Разрешить передачу cookies
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
