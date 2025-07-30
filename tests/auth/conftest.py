59ce4ec8d03ef1cee22b44f2b5fb2d860dc06f7c
Maksim <maxevg72@gmail.com>
2025-07-30 21:58:20 +0300

    featL Implemented tests for login endpoint

--- tests/auth/conftest.py
+++ tests/auth/conftest.py
@@ -3,6 +3,9 @@ from typing import Any
 import pytest
 from faker import Faker
 
+from app.domains.users.infrastructure import UserUnitOfWork
+from app.domains.users.models import User
+
 
 @pytest.fixture(scope="function")
 def register_user_data(faker: Faker) -> dict[str, Any]:
@@ -32,3 +35,15 @@ def login_data(faker: Faker):
         "email": faker.email(),
         "password": faker.password(),
     }
+
+
+@pytest.fixture(scope="function")
+async def test_user_with_data(
+    user_uow: UserUnitOfWork,
+    user_data: dict[str | Any],
+) -> dict[str, User | dict]:
+    user_creation_data = user_data.copy()
+    user_creation_data.pop("subscription_type_id")
+    user = await user_uow.user_repository.create(**user_creation_data)
+
+    return {"user": user, "user_data": user_data}
--- tests/auth/test_login.py
+++ tests/auth/test_login.py
@@ -1,3 +1,113 @@
+from datetime import datetime, timedelta, timezone
+
 import pytest
+from faker import Faker
+from httpx import AsyncClient
+from jose import jwt
+
+from app.core.config import settings
+from app.domains.users.models import User
+from tests.auth.utils import decode_jwt
 
 pytestmark = pytest.mark.anyio
+
+
+async def test_login(client: AsyncClient, test_user_with_data: dict[str, User | dict]) -> None:
+    user_data = test_user_with_data["user_data"]
+
+    response = await client.post(
+        "api/auth/login",
+        json={"email": user_data["email"], "password": user_data["password"]},
+    )
+    jwt_decoded = jwt.decode(response.json()["access_token"], settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+
+    assert response.status_code == 200
+    assert jwt_decoded["email"] == user_data["email"]
+
+
+async def test_access_token_expiry(
+    client,
+    test_user_with_data: dict[str, User | dict],
+) -> None:
+    user_data = test_user_with_data["user_data"]
+
+    response = await client.post(
+        "/api/auth/login",
+        json={
+            "email": user_data["email"],
+            "password": user_data["password"],
+            "remember_me": False,
+        },
+    )
+
+    payload = decode_jwt(response.json()["access_token"])
+
+    exp = datetime.fromtimestamp(payload["exp"], tz=timezone.utc)
+    now = datetime.now(timezone.utc)
+    expected_delta = timedelta(hours=settings.ACCESS_TOKEN_LIFESPAN_HOURS)
+
+    assert response.status_code == 200
+    assert now + expected_delta - timedelta(seconds=5) <= exp <= now + expected_delta + timedelta(seconds=5)
+
+
+async def test_refresh_token_expiry_with_remember_me(
+    client,
+    test_user_with_data: dict[str, User | dict],
+) -> None:
+    user_data = test_user_with_data["user_data"]
+
+    response = await client.post(
+        "/api/auth/login",
+        json={
+            "email": user_data["email"],
+            "password": user_data["password"],
+            "remember": True,
+        },
+    )
+
+    payload = jwt.decode(response.json()["refresh_token"], settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+    exp = datetime.fromtimestamp(payload["exp"], tz=timezone.utc)
+    now = datetime.now(timezone.utc)
+    expected_delta = timedelta(days=settings.REFRESH_TOKEN_REMEMBER_ME_LIFETIME_DAYS)
+
+    assert response.status_code == 200
+    assert now + expected_delta - timedelta(seconds=5) <= exp <= now + expected_delta + timedelta(seconds=5)
+
+
+async def test_refresh_token_expiry(
+    client,
+    test_user_with_data: dict[str, User | dict],
+) -> None:
+    user_data = test_user_with_data["user_data"]
+
+    response = await client.post(
+        "/api/auth/login",
+        json={
+            "email": user_data["email"],
+            "password": user_data["password"],
+            "remember": False,
+        },
+    )
+
+    payload = jwt.decode(response.json()["refresh_token"], settings.SECRET_KEY, algorithms=[settings.ALGORITHM])
+
+    exp = datetime.fromtimestamp(payload["exp"], tz=timezone.utc)
+    now = datetime.now(timezone.utc)
+    expected_delta = timedelta(days=settings.REFRESH_TOKEN_LIFETIME_DAYS)
+
+    assert now + expected_delta - timedelta(seconds=5) <= exp <= now + expected_delta + timedelta(seconds=5)
+
+
+async def test_user_does_not_exist(
+    client,
+    faker: Faker,
+) -> None:
+    response = await client.post(
+        "/api/auth/login",
+        json={
+            "email": faker.email(),
+            "password": faker.pystr(),
+        },
+    )
+
+    assert response.status_code == 401
