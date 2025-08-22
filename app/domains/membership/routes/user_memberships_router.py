78aa1ed12de1b087547623462db0a2227ad07a9f
Maksim <maxevg72@gmail.com>
2025-08-23 00:05:48 +0300

    feat: Implemented user membership endpoints

---
+++ app/domains/membership/routes/user_memberships_router.py
@@ -0,0 +1,52 @@
+import stripe
+from fastapi import APIRouter
+from fastapi_exception_responses import Responses
+
+from app.core.config import settings
+from app.domains.auth.utils import CurrentUserDep
+from app.domains.membership.dependencies import CurrentUserMembershipDep
+from app.domains.membership.models import MembershipStatusEnum, UserMembershipSchema
+from app.domains.membership.services import MembershipServiceDep
+
+stripe.api_key = settings.STRIPE_API_KEY
+router = APIRouter(prefix="/user-memberships", tags=["User memberships"])
+
+
+class CurrentUserMembershipResponses(Responses):
+    MEMBERSHIP_NOT_FOUND = 404, "Membership for current user not found"
+
+
+@router.get(
+    "/current-user-membership",
+    responses=CurrentUserMembershipResponses.responses,
+    summary="Get Current user membership",
+)
+async def get_current_user_membership(membership: CurrentUserMembershipDep) -> UserMembershipSchema:
+    if membership is None:
+        raise CurrentUserMembershipResponses.MEMBERSHIP_NOT_FOUND
+
+    return UserMembershipSchema.from_orm(membership)
+
+
+class CancelMembershipResponses(Responses):
+    NO_ACTIVE_MEMBERSHIP = 404, "Active membership for current user not found"
+
+
+@router.put(
+    "/current-user-membership",
+    responses=CancelMembershipResponses.responses,
+    summary="Cancel active current user membership",
+)
+async def cancel_membership(
+    current_user: CurrentUserDep,
+    service: MembershipServiceDep,
+) -> UserMembershipSchema:
+    membership = await service.get_membership_by_kwargs(user_id=current_user.id, status=MembershipStatusEnum.ACTIVE)
+
+    if membership is None:
+        raise CancelMembershipResponses.NO_ACTIVE_MEMBERSHIP
+
+    updated_membership = await service.update_membership(membership.id, {"status": MembershipStatusEnum.CANCELED})
+    stripe.Subscription.modify(membership.stripe_subscription_id, cancel_at_period_end=True)
+
+    return UserMembershipSchema.from_orm(updated_membership)
